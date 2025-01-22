584ed84a174d129c8524d8a144e4441eef41484b
Patrik Gaicean <patrik@nassautech.xyz>
2025-01-22 13:19:49 +0200

    create intial tokens

---
+++ token/token.js
@@ -0,0 +1,25 @@
+export const tokens = {
+    ILLEGAL: "ILLEGAL",
+    EOF: "EOF",
+
+    // Identifiers + literals
+    IDENT: "IDENT",
+    INT: "INT",
+
+    // Operators
+    ASIGN: "=",
+    PLUS: "+",
+
+    // Delimiters
+    COMMA: ",",
+    SEMICOLON: ";",
+
+    LPAREN: "(",
+    RPAREN: ")",
+    LBRACE: "{",
+    RBRACE: "}",
+
+    // Keywords
+    FUNCTION: "FUNCTION",
+    LET: "LET",
+}
